b8826f348fe37734fecd5b8ff93c5b9af34d34a7
issmaker <126533180+issmaker@users.noreply.github.com>
2026-07-18 21:53:33 +0300

    Expose interactive material map in UI

--- src/OptimizerEngine.h
+++ src/OptimizerEngine.h
@@ -9,6 +9,7 @@ class OptimizerEngine final : public QObject {
     Q_PROPERTY(QString sourceUrl READ sourceUrl NOTIFY sourceUrlChanged)
     Q_PROPERTY(QString resultUrl READ resultUrl NOTIFY resultUrlChanged)
     Q_PROPERTY(QString referenceUrl READ referenceUrl NOTIFY referenceUrlChanged)
+    Q_PROPERTY(QString materialUrl READ materialUrl NOTIFY materialUrlChanged)
     Q_PROPERTY(QString status READ status NOTIFY statusChanged)
     Q_PROPERTY(QString report READ report NOTIFY reportChanged)
     Q_PROPERTY(QString outputPath READ outputPath NOTIFY outputPathChanged)
@@ -20,6 +21,7 @@ public:
     explicit OptimizerEngine(QObject *parent=nullptr);
     QString sourceUrl()const{return m_sourceUrl;} QString resultUrl()const{return m_resultUrl;}
     QString referenceUrl()const{return m_referenceUrl;}
+    QString materialUrl()const{return m_materialUrl;}
     QString status()const{return m_status;} QString report()const{return m_report;}
     QString outputPath()const{return m_outputPath;}
     double progress()const{return m_progress;} bool busy()const{return m_busy;}
@@ -27,13 +29,13 @@ public:
     Q_INVOKABLE void load(const QString &url); Q_INVOKABLE void optimize(double maxMb=3.0);
     Q_INVOKABLE void openSourceFolder(); Q_INVOKABLE void openOutputFolder();
 signals:
-    void sourceUrlChanged(); void resultUrlChanged(); void referenceUrlChanged(); void statusChanged(); void reportChanged();
+    void sourceUrlChanged(); void resultUrlChanged(); void referenceUrlChanged(); void materialUrlChanged(); void statusChanged(); void reportChanged();
     void outputPathChanged();
     void progressChanged(); void busyChanged();
     void telemetryChanged();
 private:
     QString localPath()const; void setProgress(double,const QString&);
-    QString m_sourceUrl,m_resultUrl,m_referenceUrl,m_status="Перетащите PNG",m_report,m_outputPath; double m_progress=0; bool m_busy=false;
+    QString m_sourceUrl,m_resultUrl,m_referenceUrl,m_materialUrl,m_status="Перетащите PNG",m_report,m_outputPath; double m_progress=0; bool m_busy=false;
     QVariantList m_progressHistory,m_activityHistory;
     QFutureWatcher<TextureResult> m_watcher;
 };

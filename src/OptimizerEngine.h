aaf8a0b701a60ba02b1982e0cac090a80f8c5720
issmaker <126533180+issmaker@users.noreply.github.com>
2026-07-18 19:01:01 +0300

    Fix src/OptimizerEngine.h

--- src/OptimizerEngine.h
+++ src/OptimizerEngine.h
@@ -7,6 +7,7 @@ class OptimizerEngine final : public QObject {
     Q_OBJECT
     Q_PROPERTY(QString sourceUrl READ sourceUrl NOTIFY sourceUrlChanged)
     Q_PROPERTY(QString resultUrl READ resultUrl NOTIFY resultUrlChanged)
+    Q_PROPERTY(QString referenceUrl READ referenceUrl NOTIFY referenceUrlChanged)
     Q_PROPERTY(QString status READ status NOTIFY statusChanged)
     Q_PROPERTY(QString report READ report NOTIFY reportChanged)
     Q_PROPERTY(QString outputPath READ outputPath NOTIFY outputPathChanged)
@@ -15,17 +16,18 @@ class OptimizerEngine final : public QObject {
 public:
     explicit OptimizerEngine(QObject *parent=nullptr);
     QString sourceUrl()const{return m_sourceUrl;} QString resultUrl()const{return m_resultUrl;}
+    QString referenceUrl()const{return m_referenceUrl;}
     QString status()const{return m_status;} QString report()const{return m_report;}
     QString outputPath()const{return m_outputPath;}
     double progress()const{return m_progress;} bool busy()const{return m_busy;}
     Q_INVOKABLE void load(const QString &url); Q_INVOKABLE void optimize(double maxMb=3.0);
     Q_INVOKABLE void openSourceFolder(); Q_INVOKABLE void openOutputFolder();
 signals:
-    void sourceUrlChanged(); void resultUrlChanged(); void statusChanged(); void reportChanged();
+    void sourceUrlChanged(); void resultUrlChanged(); void referenceUrlChanged(); void statusChanged(); void reportChanged();
     void outputPathChanged();
     void progressChanged(); void busyChanged();
 private:
     QString localPath()const; void setProgress(double,const QString&);
-    QString m_sourceUrl,m_resultUrl,m_status="Перетащите PNG",m_report,m_outputPath; double m_progress=0; bool m_busy=false;
+    QString m_sourceUrl,m_resultUrl,m_referenceUrl,m_status="Перетащите PNG",m_report,m_outputPath; double m_progress=0; bool m_busy=false;
     QFutureWatcher<TextureResult> m_watcher;
 };

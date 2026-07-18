4a8fc1a874e859a1379cda28c71e3c83441bdc59
issmaker <126533180+issmaker@users.noreply.github.com>
2026-07-18 18:07:28 +0300

    Add src/TextureProcessor.h

---
+++ src/TextureProcessor.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <QImage>
+#include <QString>
+#include <functional>
+
+struct TextureResult {
+    QImage original;
+    QImage output;
+    QByteArray png;
+    QString report;
+    double psnr = 0;
+    double meanError = 0;
+    int maxError = 0;
+    int tested = 0;
+    bool lossless = false;
+};
+
+class TextureProcessor final {
+public:
+    using Progress = std::function<void(double, const QString &)>;
+    static TextureResult process(const QString &path, qint64 limitBytes, const Progress &progress);
+private:
+    static QImage edgeAwareCandidate(const QImage &, int smooth, int quant);
+    static void measure(const QImage &, const QImage &, double &, double &, int &);
+};

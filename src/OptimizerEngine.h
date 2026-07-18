1132fb641b52613d06016d32d45b21dd3e55e8d9
issmaker <126533180+issmaker@users.noreply.github.com>
2026-07-18 21:33:19 +0300

    Add realtime teal processing telemetry

--- src/OptimizerEngine.h
+++ src/OptimizerEngine.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <QObject>
 #include <QFutureWatcher>
+#include <QVariantList>
 #include "TextureProcessor.h"
 
 class OptimizerEngine final : public QObject {
@@ -13,6 +14,8 @@ class OptimizerEngine final : public QObject {
     Q_PROPERTY(QString outputPath READ outputPath NOTIFY outputPathChanged)
     Q_PROPERTY(double progress READ progress NOTIFY progressChanged)
     Q_PROPERTY(bool busy READ busy NOTIFY busyChanged)
+    Q_PROPERTY(QVariantList progressHistory READ progressHistory NOTIFY telemetryChanged)
+    Q_PROPERTY(QVariantList activityHistory READ activityHistory NOTIFY telemetryChanged)
 public:
     explicit OptimizerEngine(QObject *parent=nullptr);
     QString sourceUrl()const{return m_sourceUrl;} QString resultUrl()const{return m_resultUrl;}
@@ -20,14 +23,17 @@ public:
     QString status()const{return m_status;} QString report()const{return m_report;}
     QString outputPath()const{return m_outputPath;}
     double progress()const{return m_progress;} bool busy()const{return m_busy;}
+    QVariantList progressHistory()const{return m_progressHistory;} QVariantList activityHistory()const{return m_activityHistory;}
     Q_INVOKABLE void load(const QString &url); Q_INVOKABLE void optimize(double maxMb=3.0);
     Q_INVOKABLE void openSourceFolder(); Q_INVOKABLE void openOutputFolder();
 signals:
     void sourceUrlChanged(); void resultUrlChanged(); void referenceUrlChanged(); void statusChanged(); void reportChanged();
     void outputPathChanged();
     void progressChanged(); void busyChanged();
+    void telemetryChanged();
 private:
     QString localPath()const; void setProgress(double,const QString&);
     QString m_sourceUrl,m_resultUrl,m_referenceUrl,m_status="Перетащите PNG",m_report,m_outputPath; double m_progress=0; bool m_busy=false;
+    QVariantList m_progressHistory,m_activityHistory;
     QFutureWatcher<TextureResult> m_watcher;
 };

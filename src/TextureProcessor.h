8a0c6feffc529b440c3edf4edc72e6e2c4baab37
issmaker <126533180+issmaker@users.noreply.github.com>
2026-07-18 22:18:57 +0300

    Replace block pipeline with adaptive RGB24 palette

--- src/TextureProcessor.h
+++ src/TextureProcessor.h
@@ -6,7 +6,6 @@
 struct TextureResult {
     QImage original;
     QImage output;
-    QImage materialMap;
     QByteArray png;
     QString report;
     double psnr = 0;
@@ -22,11 +21,10 @@ public:
     static TextureResult process(const QString &path, qint64 limitBytes, const Progress &progress);
 private:
     static QImage edgeAwareCandidate(const QImage &, int smooth, int quant);
-    static QImage blockTextureCandidate(const QImage &, int block, int residualStep, int chromaSmooth, int chromaStep);
+    static QImage paletteCandidate(const QImage &, int colors, int preserveThreshold);
     static QImage lumaChromaCandidate(const QImage &, int lumaStep, int chromaStep);
     static QImage auditRepair(const QImage &, const QImage &, int strength, int &correctedPixels);
     static QImage guardCandidate(const QImage &, const QImage &, int rgbGuard, int chromaGuard);
     static QImage areaDownsample(const QImage &, const QSize &);
-    static QImage analyzeMaterials(const QImage &, QString &summary);
     static void measure(const QImage &, const QImage &, double &, double &, int &);
 };

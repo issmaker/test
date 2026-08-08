3fcb50171e93617f7c16bc8dbee5832e623d1298
issmaker <126533180+issmaker@users.noreply.github.com>
2026-08-08 14:57:03 +0300

    v26: update src/TextureProcessor.h

--- src/TextureProcessor.h
+++ src/TextureProcessor.h
@@ -13,7 +13,6 @@ struct TextureResult {
     int maxError = 0;
     int tested = 0;
     bool lossless = false;
-    int algorithmId = 0;
     int paletteColors = 0;
     QString algorithmName;
 };
@@ -21,15 +20,9 @@ struct TextureResult {
 class TextureProcessor final {
 public:
     using Progress = std::function<void(double, const QString &)>;
-    static TextureResult process(const QString &path, qint64 limitBytes, int algorithmId, const Progress &progress);
+    static TextureResult process(const QString &path, qint64 limitBytes, const Progress &progress);
 private:
-    static QImage edgeAwareCandidate(const QImage &, int smooth, int quant);
-    static QImage paletteCandidate(const QImage &, int colors, int preserveThreshold);
     static QImage perceptualPaletteCandidate(const QImage &, int colors, int model, int orderedStrength);
-    static QImage jpegBridgeCandidate(const QImage &, int quality);
-    static QImage lumaChromaCandidate(const QImage &, int lumaStep, int chromaStep);
-    static QImage auditRepair(const QImage &, const QImage &, int strength, int &correctedPixels);
-    static QImage guardCandidate(const QImage &, const QImage &, int rgbGuard, int chromaGuard);
     static QImage areaDownsample(const QImage &, const QSize &);
     static void measure(const QImage &, const QImage &, double &, double &, int &);
 };

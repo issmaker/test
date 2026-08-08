faa941949b38f8195b57b52c82b42d9f68b3f666
issmaker <126533180+issmaker@users.noreply.github.com>
2026-08-08 17:56:44 +0300

    v30 Liquid Glass: update src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -65,7 +65,7 @@ int runSelfTest() {
     if (!directory.isValid()) return 2;
 
     QImage input(512, 512, QImage::Format_RGB888);
-    quint32 state = 0x29a6f19dU;
+    quint32 state = 0x30a6f19dU;
     for (int y = 0; y < input.height(); ++y) {
         uchar *line = input.scanLine(y);
         for (int x = 0; x < input.width(); ++x) {
@@ -96,15 +96,15 @@ int runSelfTest() {
 } // namespace
 
 int main(int argc,char**argv){
-    QGuiApplication app(argc,argv);app.setApplicationName("Adaptive Texture Optimizer");app.setApplicationVersion("29");
-    app.setWindowIcon(QIcon(QStringLiteral(":/icons/mars.svg")));
+    QGuiApplication app(argc,argv);app.setApplicationName("Adaptive Texture Optimizer");app.setApplicationVersion("30");
+    app.setWindowIcon(QIcon(QStringLiteral(":/icons/liquid.svg")));
     g_startupLogPath = QCoreApplication::applicationDirPath()
         + QStringLiteral("/AdaptiveTextureOptimizer-startup.log");
     {
         QFile log(g_startupLogPath);
         if (log.open(QIODevice::WriteOnly | QIODevice::Truncate | QIODevice::Text)) {
             QTextStream out(&log);
-            out << "Adaptive Texture Optimizer 29 startup\n";
+            out << "Adaptive Texture Optimizer 30 startup\n";
             out << "Qt " << qVersion() << "\n";
         }
     }

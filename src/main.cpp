bc61af9d2da5a32e22c94a486b97b534d94bdbf5
issmaker <126533180+issmaker@users.noreply.github.com>
2026-08-08 23:05:30 +0300

    feat(v32): update src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -67,7 +67,7 @@ int runSelfTest() {
     if (!directory.isValid()) return 2;
 
     QImage input(512, 512, QImage::Format_RGB888);
-    quint32 state = 0x31a6f19dU;
+    quint32 state = 0x32a6f19dU;
     for (int y = 0; y < input.height(); ++y) {
         uchar *line = input.scanLine(y);
         for (int x = 0; x < input.width(); ++x) {
@@ -107,7 +107,7 @@ int runSelfTest() {
 } // namespace
 
 int main(int argc,char**argv){
-    QGuiApplication app(argc,argv);app.setApplicationName("Adaptive Texture Optimizer");app.setApplicationVersion("31");
+    QGuiApplication app(argc,argv);app.setApplicationName("Adaptive Texture Optimizer");app.setApplicationVersion("32");
     app.setWindowIcon(QIcon(QStringLiteral(":/icons/liquid.svg")));
     g_startupLogPath = QCoreApplication::applicationDirPath()
         + QStringLiteral("/AdaptiveTextureOptimizer-startup.log");
@@ -115,7 +115,7 @@ int main(int argc,char**argv){
         QFile log(g_startupLogPath);
         if (log.open(QIODevice::WriteOnly | QIODevice::Truncate | QIODevice::Text)) {
             QTextStream out(&log);
-            out << "Adaptive Texture Optimizer 31 startup\n";
+            out << "Adaptive Texture Optimizer 32 startup\n";
             out << "Qt " << qVersion() << "\n";
         }
     }
